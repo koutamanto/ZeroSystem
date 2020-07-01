2019649a625524e37b98f2074c4d821a80169520
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-01 19:59:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template
-app = Flask(__name__) #インスタンス生成
+app = Flask(__name__)
 
-@app.route("/") #アリケーションルートにアクセスが合った場合
-def hello(): #hello関数が動作します。
-  return "Hello World!" #ブラウザ画面に"Hello World!"と出力されます。
+@app.route("/")
+def hello():
+  return "Hello World!"
 
 @app.route("/index")
 def index():
@@ -18,5 +18,4 @@ def index():
     return render_template('index.html', message=message)
 
 if __name__ == "__main__":
-    # webサーバー立ち上げ
     app.run()
