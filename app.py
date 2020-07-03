765fa2f9027d50251d6b7c07f519a5086401a6b7
koutamanto <58100773+koutamanto@users.noreply.github.com>
2020-07-03 15:39:24 +0900

    no message

--- app.py
+++ app.py
@@ -1,21 +1,70 @@
 # coding: utf-8
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask,request,render_template
+import time
+import urllib.request, urllib.error
+from apiclient.discovery import build
+from apiclient.errors import HttpError
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+url = []
+t = []
+
+DEVELOPER_KEY = 'AIzaSyC-daF5wvITHzm9y5pIHPvZiRfSYUW71xY'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+youtube = build(
+    YOUTUBE_API_SERVICE_NAME, 
+    YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY
+    )
+
+app = Flask(__name__)
 
 @app.route("/index")
 def index():
-    html = requests.get('https://www.timeanddate.com/worldclock/').text
-    soup = BeautifulSoup(html)
-    time = soup.find(id='c248').text
-    print(time)
-    message = time
-    return render_template('index.html', message=message)
+    search_response = youtube.search().list(
+        q='ヒカキン',
+        part='id,snippet',
+        maxResults=50,
+        ).execute()
 
+    items = search_response["items"]
+
+    for item in items:
+        title = item["snippet"]["title"]
+        print(title)
+        thumb = item["snippet"]["thumbnails"]["high"]['url']
+        print(thumb)
+        url.append(thumb)
+        t.append(title)
+        print(t)
+        print(url)
+    message = 'aaa'
+    return render_template('index.html', data=zip(t,url))
+
+@app.route("/result", methods=["post"])
+def result():
+    word = request.form["word"]
+    search_response = youtube.search().list(
+        q=word,
+        part='id,snippet',
+        maxResults=50,
+        ).execute()
+
+    items = search_response["items"]
+
+    for item in items:
+        title = item["snippet"]["title"]
+        print(title)
+        thumb = item["snippet"]["thumbnails"]["high"]['url']
+        print(thumb)
+        url.append(thumb)
+        t.append(title)
+        print(t)
+        print(url)
+    message = 'aaa'
+    return render_template('index.html', data=zip(t,url))
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
